bb80fde108a4d596be58dc3c146accad79b7b4ec
IllyaShramko <123illya123123r@gmail.com>
2026-01-11 15:45:56 +0200

    Fixed problems

--- src/shared/images.ts
+++ src/shared/images.ts
@@ -2,6 +2,7 @@ import About1 from "./../assets/images/about1.jpg"
 import About2 from "./../assets/images/about2.jpg"
 import About3 from "./../assets/images/about3.jpg"
 import Logo from "./../assets/images/logo.png"
+import bigLogo from "./../assets/images/bigLogo.png"
 export const IMAGES = {
-    About1, About2, About3, Logo
+    About1, About2, About3, Logo, bigLogo
 }
