d00bb2c01c6d4f717df6952ea68bf5bd64be83dc
IllyaShramko <123illya123123r@gmail.com>
2026-02-15 19:35:01 +0200

    Contacts page in work #1

--- src/app/AppRoutes.tsx
+++ src/app/AppRoutes.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Layout } from './layout';
-import { AboutPage, CatalogPage, HomePage, NotFoundPage, ProductPage } from '../pages';
+import { AboutPage, CatalogPage, ContactsPage, HomePage, NotFoundPage, ProductPage } from '../pages';
 export function AppRoutes() {
     return <BrowserRouter>
         <Routes>
@@ -9,6 +9,7 @@ export function AppRoutes() {
                 <Route path='/catalog' element={<CatalogPage/>}/>
                 <Route path='/' element={<HomePage/>}/>
                 <Route path='/products/:id' element={<ProductPage/>}></Route>
+                <Route path='/contacts' element={<ContactsPage/>}></Route>
                 <Route path='/*' element={<NotFoundPage/>}/>
             </Route>
         </Routes>
---
+++ src/pages/contacts/ContactsPage.tsx
@@ -0,0 +1,43 @@
+import styles from "./contacts.module.css"
+import { ICONS } from "../../shared/icons"
+import { Link } from "react-router-dom"
+export function ContactsPage() {
+    return <div className={styles.page}>
+        <div className={styles.header}>
+            <h1>Контакти</h1>
+        </div>
+        <div className={styles.content}>
+            <div className={styles.info}>
+                <div>
+                    <h3 className={styles.title}>Наші контакти</h3>
+                </div>
+                <div>
+                    <div>
+                        <ICONS.Phone/>
+                        <p>+38 (067) 123-45-67</p>
+                    </div>
+                    <div>
+                        <ICONS.Mail/>
+                        <p>info@dronex.com.ua</p>
+                    </div>
+                    <div>
+                        <ICONS.PinMap/>
+                        <p>вул. Університетська, 22, м. Дніпро, 49000, Україна</p>
+                    </div>
+                    <div>
+                        <ICONS.Calendar/>
+                        <p>Пн–Пт: 10:00 — 18:00, Сб–Нд: вихідні</p>
+                    </div>
+                </div>
+                <div>
+                    <h4 className={styles.socialTitle}>Ми в соціальних мережах</h4>
+                    <div className={styles.socialIcons}>
+                        <Link to={"#"}><ICONS.Facebook/></Link>
+                        <Link to={"#"}><ICONS.Telegram/></Link>
+                        <Link to={"#"}><ICONS.Instagram/></Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+}
